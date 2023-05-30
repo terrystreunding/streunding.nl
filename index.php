3bfef7a28d71ea4d71f6002e42d7104368474845
Terry Streunding <terry@gogento.com>
2023-05-30 11:00:02 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Yara's Creations</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+
+        .header {
+            background-image: url("pics/top.jpg");
+            background-size: cover;
+            height: 450px;
+            text-align: center;
+            color: white;
+            font-size: 24px;
+            padding-top: 200px;
+        }
+
+        .header a {
+            color: white;
+            text-decoration: none;
+            font-weight: bold;
+            display: block;
+            margin-bottom: 10px;
+        }
+
+        .text {
+            text-align: center;
+            color: black;
+            text-decoration: none;
+            font-weight: bold;
+            display: block;
+            margin-bottom: 10px;
+        }
+        .text a {
+            color: black;
+            text-decoration: none;
+            font-weight: bold;
+            display: block;
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
+<div class="header">
+    <h1>Yara's Creations</h1>
+</div>
+<div class="text">
+    <h2><a href="https://streunding.nl/wordpress/">Ga naar onze blog</a></h2>
+    <p>Korte beschrijving van de blogpost. Maximaal 50 woorden.</p>
+    <h2><a href="https://streunding.nl/wordpress/winkel/">Ga naar mijn Winkel</a></h2>
+    <p>Korte beschrijving van de blogpost. Maximaal 50 woorden.</p>
+    <h2>    <a href="https://patreon.com/user?u=93813530">Steun me op Patreon</a></h2>
+    <p>Korte beschrijving van de blogpost. Maximaal 50 woorden.</p>
+</div>
+</body>
+</html>
